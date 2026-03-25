63c73f9ca3192240bb38d50a31c5fbc5891e7b10
BMartiin <baloghmartin.prs@gmail.com>
2026-03-25 15:30:47 +0100

    added cold coin mechanic

--- Assets/Scripts/Managers/CodingManager.cs
+++ Assets/Scripts/Managers/CodingManager.cs
@@ -17,23 +17,29 @@ public class CodingManager : MonoBehaviour
     public TMP_Text taskDescriptionText;
     public TMP_InputField inputField;
     public TMP_Text testStatusLabel;
+    public TMP_Text goldCountText;
 
     [Header("Adatok")]
     public List<QuestionData> questions;
     public GameObject knightPrefab;
-    public Transform spawnPoint;
+    public List<Transform> spawnPoints;
 
     private Engine engine;
     private int currentQuestionIndex = 0;
-    private int spawnedCount = 0;
+    private int internalCodeActionCount = 0;
     private int lastPowerValue = 0;
-    private int gold = 150;
+    private int goldCount = 0;
+    private bool[] completedQuestions;
+    private string[] rightAnswers;
 
     private void Start()
     {
         Time.timeScale = 0.0f;
         puzzlePanel.SetActive(true);
+
+        RefreshAnswers();
         InitEngine();
+        UpdateGoldCount();
     }
 
     private void InitEngine()
@@ -41,12 +47,13 @@ public class CodingManager : MonoBehaviour
         engine = new Engine();
 
         engine.SetValue("lovag", new Action<object>((p) => {
+            internalCodeActionCount++;
             int power = 1;
             if (p != null) int.TryParse(p.ToString(), out power);
-            SpawnKnight(power);
+            lastPowerValue = power;
         }));
 
-        engine.SetValue("arany", gold);
+        engine.SetValue("arany", 100);
         SelectQuestion(0);
     }
 
@@ -54,28 +61,33 @@ public class CodingManager : MonoBehaviour
     {
         if (index >= 0 && index < questions.Count)
         {
+
             currentQuestionIndex = index;
             taskDescriptionText.text = questions[index].Description;
-            inputField.text = "";
-            testStatusLabel.text = "H/N";
-            testStatusLabel.color = Color.white;
+
+            if (completedQuestions[index])
+            {
+                inputField.text = rightAnswers[index];
+                inputField.interactable = false;
+                testStatusLabel.text = "SIKER!";
+                testStatusLabel.color = Color.green;
+            }
+            else
+            {
+                inputField.text = "";
+                inputField.interactable = true;
+                testStatusLabel.text = "H/N";
+                testStatusLabel.color = Color.white;
+            }
         }
-    }
-
-    public void SpawnKnight(int power)
-    {
-        spawnedCount++;
-        lastPowerValue = power;
-
-        Quaternion rightRotation = Quaternion.Euler(0, 180, 0);
-        Instantiate(knightPrefab, spawnPoint.position, rightRotation);
     }
 
     public void CheckCode()
     {
-        spawnedCount = 0;
+        if (completedQuestions[currentQuestionIndex]) return;
+
+        internalCodeActionCount = 0;
         lastPowerValue = 0;
-        engine.SetValue("arany", gold);
 
         try
         {
@@ -95,15 +107,22 @@ public class CodingManager : MonoBehaviour
 
         switch (currentQuestionIndex)
         {
-            case 0: success = (spawnedCount == 1); break;
-            case 1: success = (spawnedCount == 3); break;
-            case 2: success = (spawnedCount == 1); break;
+            case 0: success = (internalCodeActionCount == 1); break;
+            case 1: success = (internalCodeActionCount == 3); break;
+            case 2: success = (internalCodeActionCount == 1); break;
             case 3: success = (lastPowerValue == 10); break;
-            case 4: success = (spawnedCount == 2); break;
+            case 4: success = (internalCodeActionCount == 2); break;
         }
 
         if (success)
         {
+            completedQuestions[currentQuestionIndex] = true;
+            rightAnswers[currentQuestionIndex] = inputField.text;
+            inputField.interactable = false;
+
+            goldCount++;
+            UpdateGoldCount();
+
             testStatusLabel.text = "SIKER!";
             testStatusLabel.color = Color.green;
         }
@@ -116,6 +135,9 @@ public class CodingManager : MonoBehaviour
 
     public void StartBattle()
     {
+     
+        SpawnKnights();
+        
         Time.timeScale = 1.0f;
         puzzlePanel.SetActive(false);
     }
@@ -124,5 +146,27 @@ public class CodingManager : MonoBehaviour
     {
         Time.timeScale = 1.0f;
         puzzlePanel.SetActive(false);
+    }
+
+    private void UpdateGoldCount()
+    {
+        goldCountText.text = goldCount.ToString();
+    }
+
+    private void RefreshAnswers()
+    {
+        completedQuestions = new bool[questions.Count];
+        rightAnswers = new string[questions.Count];
+    }
+
+    private void SpawnKnights()
+    {
+        for (int i = 0; i < goldCount; i++)
+        {
+            if (i < spawnPoints.Count)
+            {
+                Instantiate(knightPrefab, spawnPoints[i].position, Quaternion.Euler(0,180,0));
+            }
+        }
     }
 }
